76346d94c67835eef52c63e04c51d7c8919008a1
lxxjn0 <ljy3510@gmail.com>
2020-06-09 01:11:44 +0900

    feat : 결과를 저장하는 GameResult 객체 구현
    - 게임 결과를 log로 반환해주는 메서드 구현.

---
+++ src/main/java/me/lxxjn0/game/domain/baseballGame/GameResult.java
@@ -0,0 +1,56 @@
+package me.lxxjn0.game.domain.baseballGame;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import me.lxxjn0.game.domain.baseball.Baseballs;
+
+public class GameResult {
+
+	private static final String NOTHING_LOG = "낫싱";
+	private static final String STRIKE_LOG = "스트라이크";
+	private static final String BALL_LOG = "볼";
+	private static final String DELIMITER = ", ";
+
+	private final int strikeCounts;
+	private final int ballCounts;
+
+	private GameResult(final int strikeCounts, final int ballCounts) {
+		this.strikeCounts = strikeCounts;
+		this.ballCounts = ballCounts;
+	}
+
+	public static GameResult of(final Baseballs target, final Baseballs guess) {
+		final int strikeCounts = guess.countStrikes(target);
+		final int ballCounts = guess.countBalls(target);
+
+		return new GameResult(strikeCounts, ballCounts);
+	}
+
+	public String log() {
+		if (isNothing()) {
+			return NOTHING_LOG;
+		}
+
+		return String.join(DELIMITER, logGameResult());
+	}
+
+	private boolean isNothing() {
+		return strikeCounts == 0 && ballCounts == 0;
+	}
+
+	private List<String> logGameResult() {
+		final List<String> gameLog = new ArrayList<>();
+
+		if (strikeCounts != 0) {
+			gameLog.add(strikeCounts + STRIKE_LOG);
+		}
+
+		if (ballCounts != 0) {
+			gameLog.add(ballCounts + BALL_LOG);
+		}
+
+		return gameLog;
+	}
+
+}
---
+++ src/test/java/me/lxxjn0/game/domain/baseballGame/GameResultTest.java
@@ -0,0 +1,34 @@
+package me.lxxjn0.game.domain.baseballGame;
+
+import static org.assertj.core.api.Assertions.*;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import me.lxxjn0.game.domain.baseball.Baseballs;
+
+class GameResultTest {
+
+	@DisplayName("of() - 사용자가 입력한 Baseballs와 타겟 Baseballs를 입력받아 객체 생성")
+	@Test
+	void of_GuessBaseballsAndTargetBaseballs_GenerateInstance() {
+		final Baseballs target = Baseballs.of(2, 3, 4);
+		final Baseballs guess = Baseballs.of(1, 2, 3);
+
+		assertThat(GameResult.of(target, guess)).isInstanceOf(GameResult.class);
+	}
+
+	@DisplayName("log() - 게임 결과를 로그로 반환")
+	@Test
+	void log_GameResult_ReturnLogByString() {
+		final Baseballs target = Baseballs.of(1, 2, 3);
+		final Baseballs oneStrikeTwoBall = Baseballs.of(1, 3, 2);
+		final Baseballs oneBall = Baseballs.of(3, 5, 9);
+		final Baseballs nothing = Baseballs.of(4, 5, 6);
+
+		assertThat(GameResult.of(target, oneStrikeTwoBall).log()).isEqualTo("1스트라이크, 2볼");
+		assertThat(GameResult.of(target, oneBall).log()).isEqualTo("1볼");
+		assertThat(GameResult.of(target, nothing).log()).isEqualTo("낫싱");
+	}
+
+}
